5052c5ed3262c389e975ec17cd08e0524afcf5da
JULIAN <julianore79@gmail.com>
2025-05-13 10:25:48 -0500

    aa

--- src/view/js/sesion.js
+++ src/view/js/sesion.js
@@ -35,7 +35,8 @@ async function iniciar_sesion() {
                 method: 'POST',
                 mode: 'cors',
                 cache: 'no-cache',
-                body: formData
+                body: formData,
+                timer: 1500
             });
             location.replace(base_url);
             location.replace(base_url);
